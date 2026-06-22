39c655052787fae43e9a8e5f1f05906fba32e278
SIMO Yann <simoyann2004@gmail.com>
2026-06-22 10:32:26 +0100

    fix

--- backend/AuthService/src/main/java/afb/astyann/authservice/service/impl/AuthServiceImpl.java
+++ backend/AuthService/src/main/java/afb/astyann/authservice/service/impl/AuthServiceImpl.java
@@ -61,7 +61,7 @@ public class AuthServiceImpl implements IAuthService {
         return RegisterResponseDTO.builder()
                 .userId(user.getUserId())
                 .email(user.getEmail())
-                .isVerified(user.getIsVerified())
+                .isVerified(user.isVerified())
                 .build();
     }
 
