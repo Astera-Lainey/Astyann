6d22adfc66cc1742cf1d66e63d97208f83efc316
SIMO Yann <simoyann2004@gmail.com>
2026-06-23 10:42:41 +0100

    added ai orchestator service

---
+++ backend/AIOrchestrator/src/main/java/afb/astyann/aiorchestrator/AiOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package afb.astyann.aiorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiOrchestratorApplication.class, args);
+    }
+
+}
