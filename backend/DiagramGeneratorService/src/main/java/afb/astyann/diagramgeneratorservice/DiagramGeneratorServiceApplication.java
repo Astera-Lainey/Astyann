cbf7484612f9a1eda5249074580d24b7fa47be39
SIMO Yann <simoyann2004@gmail.com>
2026-07-06 15:46:28 +0100

    added diagram generator service

---
+++ backend/DiagramGeneratorService/src/main/java/afb/astyann/diagramgeneratorservice/DiagramGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package afb.astyann.diagramgeneratorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagramGeneratorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagramGeneratorServiceApplication.class, args);
+    }
+
+}
